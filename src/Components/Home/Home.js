c9502bb93381ef8895e2d058da31640ff319914f
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-01-31 10:00:09 -0600

    Sass syntax

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Home.scss'
 import LogoTitle from '../../assets/images/logo-s.png'
 import { Link } from 'react-router-dom'
@@ -9,15 +9,21 @@ const Home = () => {
   const nameArray = ['a', 'n']
   const jobArray = ['s', 'o', 'f', 't', 'w', 'a', 'r', 'e', '', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r']
 
+  useEffect(() => {
+    return setLetterClass('text-animate-hover')
+  }, [])
+  
+
+
   return (
     <div className='container home-page'>
       <div className='text-zone'>
         <h1>
-          <span clasName={letterClass}>H</span>
-          <span clasName={`${letterClass} _12`}>i,</span>
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
           <br />
-          <span clasName={`${letterClass} _13`}>I</span>
-          <span clasName={`${letterClass} _14`}>'m</span>
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
         <img src={LogoTitle} alt='developer' />
         <AnimatedLetters letterClass={letterClass} strArray={nameArray} index={15}/>
         <br />
