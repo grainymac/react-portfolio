23850f3116b90791540b3d64367a3fcbd21e48d8
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-02-03 21:18:21 -0600

    Email form

--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,14 +1,35 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import './Contact.scss'
 import Loader from 'react-loaders'
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
+    const refForm = useRef()
 
     useEffect(() => {
         setLetterClass('text-animate-hover')
     }, [])
+
+    const sendEmail = (event) => {
+        event.preventDefault()
+
+        emailjs
+            .sendForm(
+                'gmail',
+                'template_j34068y',
+                refForm.current,
+                'service_smkz3xo'
+            )
+            .then(() => {
+                    getAllByPlaceholderText('Message successfully sent!')
+                    window.location.reload(false)
+                },() => {
+                    alert('Failed to send the message, please try again!')
+                }
+            )
+    }
 
   return (
     <>
@@ -21,7 +42,7 @@ const Contact = () => {
                     I am interested in freelance opportunities, especially creative and ambitious projects. If you have any other requests or questions, please do not hesitate to get in touch with me via the form below.
                 </p>
                 <div className='contact-form'>
-                    <form>
+                    <form ref={refForm} onSubmit={sendEmail}>
                         <ul>
                             <li className='half'>
                                 <input type='text' name='name' placeholder='Name' required />
