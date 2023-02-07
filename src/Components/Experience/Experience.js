f3a11e40c0117092a601de7607194d0bb35e2dba
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-02-07 16:17:59 -0600

    Experiences

---
+++ src/Components/Experience/Experience.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Experience.scss'
+
+const Experience = () => {
+  return (
+    <div>
+        <h5>What Skills I Have</h5>
+        <h2>My Experience</h2>
+        <div className='container experience-container'>
+            <div className='experience-frontend'>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Experience
