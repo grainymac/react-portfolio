da57d70f78d1c123707a3cc50df1a533c36db4f6
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-02-21 08:28:34 -0600

    adding blog

--- src/Components/WorkCard/WorkCard.js
+++ src/Components/WorkCard/WorkCard.js
@@ -8,9 +8,30 @@ const WorkCard = () => {
     return (
         <div className='work-card-container'>
             <div>
-                <img src={theBored} alt='the bored logo' />
+                <a href=''><img src={rancid} alt='rancid logo' /></a>
+                <h2>DiscoverIt</h2>
+            </div>
+            <div>
+                <a href=''><img src={theBored} alt='the bored logo' /></a>
                 <h2>the Bored</h2>
             </div>
+            <div>
+                <a href=''><img src={dokes} alt='dokes logo' /></a>
+                <h2>Dokes</h2>
+            </div>
+            <div>
+                <a href=''><img src={rancid} alt='rancid logo' /></a>
+                <h2>Rancid Tomatillos</h2>
+            </div>
+            <div>
+                <a href=''><img src={rancid} alt='rancid logo' /></a>
+                <h2>Hotel OverLook</h2>
+            </div>
+            <div>
+                <a href=''><img src={rancid} alt='rancid logo' /></a>
+                <h2>What's Cookin'</h2>
+            </div>
+
         </div>
     )
 }
