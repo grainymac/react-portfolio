6a6b193d1f8072be4860f232ffa91229aa7b7b9e
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-03-21 15:23:32 -0500

    Refactor

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -13,37 +13,23 @@ const About = () => {
     return (
         <>
             <div className='container about-page'>
-                <div className='text-zone about-zone'>
+                <div className='text-zone about-zone about'>
                     <h1>
                         <AnimatedLetters letterClass={letterClass} strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']} idx={15} />
                     </h1>
                     <p> 
-                        &nbsp;&nbsp;&nbspHey, whats up?! I'm a front-end developer with a passion for creating beautiful and functional user interfaces using React.js and SCSS. Also as a creative with diverse skills and interests, I have the ability to explore and express your ideas through a variety of mediums, each informing and enhancing the others. With my background in graphic design, I bring a unique perspective to my work and enjoy exploring new ways to integrate design into my coding projects. I'm a highly creative person who is always looking for ways to think outside the box and push my boundaries. My dream is to work on a team that develops solutions and applications that have a positive impact on people's lives. I recently graduated from a coding bootcamp called Turing School of Software & Design and am excited to continue learning and growing in my career as a developer. 
+                        &nbsp;&nbsp;&nbsp;Hey, whats up?! I'm a front-end developer with a passion for creating beautiful and functional user interfaces using React.js and SCSS. Also as a creative with diverse skills and interests, I have the ability to explore and express your ideas through a variety of mediums, each informing and enhancing the others. With my background in graphic design, I bring a unique perspective to my work and enjoy exploring new ways to integrate design into my coding projects. I'm a highly creative person who is always looking for ways to think outside the box and push my boundaries. My dream is to work on a team that develops solutions and applications that have a positive impact on people's lives. I recently graduated from a coding bootcamp called Turing School of Software & Design and am excited to continue learning and growing in my career as a developer. 
                     </p>
                     <p>
                         &nbsp;&nbsp;&nbsp;I started my journey as a software developer by attending a coding bootcamp at Turing School of Software & Design, where I learned new skills remotely. Despite the challenges of the fast-paced learning environment, I found the experience extremely rewarding, with a strong emphasis on community-based learning. During Turing, I learned Javascript and some other techonologies like React.js, GSAP, and Material UI for the first time. After graduating, you faced the challenge of finding a job in the tech field so I've been keeping busy developing my own personal applications and websites. Despite this hurdle, I remain ambitious and committed to continuing my learning and pushing forward in my career as a software developer.
-
                     </p>
                     <h2>
                         Extras:
                     </h2>
-                    <div className='skills-area'>
-                        <img src='https://img.shields.io/badge/Adobe%20Illustrator-FF9A00?style=for-the-badge&logo=adobe%20illustrator&logoColor=white' alt='illustrator' />
-                        <img src='https://img.shields.io/badge/Adobe%20Photoshop-31A8FF?style=for-the-badge&logo=Adobe%20Photoshop&logoColor=black' alt='photoshop' />
-                        <img src='https://img.shields.io/badge/Adobe%20Lightroom-31A8FF?style=for-the-badge&logo=Adobe%20Lightroom&logoColor=white' alt='lightroom' />
-                        <img src='https://img.shields.io/badge/Adobe%20InDesign-FF3366?style=for-the-badge&logo=Adobe%20InDesign&logoColor=white' alt='indesign' />
-                        <img src='https://img.shields.io/badge/Trello-0052CC?style=for-the-badge&logo=trello&logoColor=white' alt='trello' />
-                        <img src='https://img.shields.io/badge/Notion-000000?style=for-the-badge&logo=notion&logoColor=white' alt='notion' />
-                        <img src='https://img.shields.io/badge/Miro-050038?style=for-the-badge&logo=Miro&logoColor=white' alt='miro' />
-                        <img src='https://img.shields.io/badge/Microsoft_Office-D83B01?style=for-the-badge&logo=microsoft-office&logoColor=white' alt='microsoft office' />
-                        <img src='https://img.shields.io/badge/Google%20Sheets-34A853?style=for-the-badge&logo=google-sheets&logoColor=white' alt='google sheets' />
-                        <img src='https://img.shields.io/badge/Figma-F24E1E?style=for-the-badge&logo=figma&logoColor=white' alt='figma' />
-                    </div>
                 </div>
             </div>
             <Loader type='ball-scale-ripple-multiple' />
         </>
     )
 }
-
 export default About
--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -6,51 +6,8 @@ import About from '../About/About'
 import Work from '../Work/Work'
 import Contact from '../Contact/Contact'
 import Skills from '../Skills/Skills';
-import Blog from '../Blog/Blog'
-import { useQuery, gql } from '@apollo/client'
-// import { GraphQLClient, gql } from 'graphql-request'
-
-// const graphCMS = new GraphQLClient('https://us-west-2.cdn.hygraph.com/content/clede0qu54mq801t6058sd0e0/master')
-
-const QUERY = gql`
-    {
-        posts {
-            id,
-            title,
-            datePublished,
-            slug,
-            content {
-                html
-            }
-            author {
-                name,
-                avatar {
-                    url
-                }
-            }
-            coverPhoto {
-                publishedAt {
-                    createdBy {
-                        id
-                    }
-                    url
-                }
-            }
-        }
-    }
-`
-
-
-
-
 
 const App = () => {
-//   const { data, loading, error } = useQuery(QUERY)
-
-//   if (loading) return 'loading...'
-//   if (error) return <pre>{error.message}</pre>
-
-//   console.log(data)
 
   return (
     <>
@@ -61,7 +18,6 @@ const App = () => {
         <Route path='/work' element={<Work />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/skills' element={<Skills />} />
-        <Route path='/blog' element={<Blog />} />
       </Route>
     </Routes>
     </>
