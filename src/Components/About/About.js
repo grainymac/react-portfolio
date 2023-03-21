3acf89b9a242fe5bdf7a987434af0d534934652c
Ian McIntosh <ian.c.mcintosh87@gmail.com>
2023-03-21 14:09:57 -0500

    finished with adding summary to about me

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -18,14 +18,11 @@ const About = () => {
                         <AnimatedLetters letterClass={letterClass} strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']} idx={15} />
                     </h1>
                     <p> 
-                        &nbsp;&nbsp;&nbspHey, whats up?! I'm a front-end developer with a passion for creating beautiful and functional user interfaces using React.js and SCSS. With a background in graphic design, I bring a unique perspective to my work and enjoy exploring new ways to integrate design into my coding projects. I'm a highly creative person who is always looking for ways to think outside the box and push my boundaries. My dream is to work on a team that develops solutions and applications that have a positive impact on people's lives. I recently graduated from a coding bootcamp called Turing School of Software & Design and am excited to continue learning and growing in my career as a developer. 
+                        &nbsp;&nbsp;&nbspHey, whats up?! I'm a front-end developer with a passion for creating beautiful and functional user interfaces using React.js and SCSS. Also as a creative with diverse skills and interests, I have the ability to explore and express your ideas through a variety of mediums, each informing and enhancing the others. With my background in graphic design, I bring a unique perspective to my work and enjoy exploring new ways to integrate design into my coding projects. I'm a highly creative person who is always looking for ways to think outside the box and push my boundaries. My dream is to work on a team that develops solutions and applications that have a positive impact on people's lives. I recently graduated from a coding bootcamp called Turing School of Software & Design and am excited to continue learning and growing in my career as a developer. 
                     </p>
                     <p>
                         &nbsp;&nbsp;&nbsp;I started my journey as a software developer by attending a coding bootcamp at Turing School of Software & Design, where I learned new skills remotely. Despite the challenges of the fast-paced learning environment, I found the experience extremely rewarding, with a strong emphasis on community-based learning. During Turing, I learned Javascript and some other techonologies like React.js, GSAP, and Material UI for the first time. After graduating, you faced the challenge of finding a job in the tech field so I've been keeping busy developing my own personal applications and websites. Despite this hurdle, I remain ambitious and committed to continuing my learning and pushing forward in my career as a software developer.
 
-                    </p>
-                    <p>
-                        &nbsp;&nbsp;&nbsp;Right now in my life, I do a lot of music production work for myself and other artists and I do photography. I've been doing these things on a professional level alongside my main career. I went to school for graphic design to also supplement my creative projects and give me a chance to build a portfolio with different art mediums.
                     </p>
                     <h2>
                         Extras:
